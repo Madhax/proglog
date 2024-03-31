93bb58ceb2013cb4f901bcf4c503e84e0467aa82
Madhax <patrick.gryciuk@gmail.com>
2024-03-31 15:44:17 -0400

    Add commentary

--- internal/log/log.go
+++ internal/log/log.go
@@ -35,12 +35,18 @@ func NewLog(dir string, c Config) (*Log, error) {
 	return l, l.setup()
 }
 
+/*
+Setup creates initial segment or reads existing segments on disk
+by reading offsets in sorted order from oldest to newest
+*/
+
 func (l *Log) setup() error {
 	files, err := os.ReadDir(l.Dir)
 	if err != nil {
 		return err
 	}
 	var baseOffsets []uint64
+	// Each filename is assumed to have numerical file name and represent the base offset
 	for _, file := range files {
 		offStr := strings.TrimSuffix(
 			file.Name(),
@@ -49,9 +55,12 @@ func (l *Log) setup() error {
 		off, _ := strconv.ParseUint(offStr, 10, 0)
 		baseOffsets = append(baseOffsets, off)
 	}
+	//sort by oldest to newest
 	sort.Slice(baseOffsets, func(i, j int) bool {
 		return baseOffsets[i] < baseOffsets[j]
 	})
+
+	// Create new segment based on offset
 	for i := 0; i < len(baseOffsets); i++ {
 		if err = l.newSegment(baseOffsets[i]); err != nil {
 			return err
@@ -59,6 +68,8 @@ func (l *Log) setup() error {
 		// baseOffset contains dup for index and store so we skip the dup
 		i++
 	}
+
+	// create new segment if none exists
 	if l.segments == nil {
 		if err = l.newSegment(
 			l.Config.Segment.InitialOffset,
@@ -69,6 +80,7 @@ func (l *Log) setup() error {
 	return nil
 }
 
+// Append record to active segment.
 func (l *Log) Append(record *api.Record) (uint64, error) {
 	l.mu.Lock()
 	defer l.mu.Unlock()
